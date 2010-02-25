9baa51d325c702186adb08d63ace847892c80487
Friedrich Romstedt <friedrichromstedt@gmail.com>
2010-02-25 21:45:23 +0100

    Rereleasing Friedrich Romstedt's files under MIT license.
    Friedrich Romstedt (me) published the files __init__.py and
    dynload.py under the GPL first, and rereleases them now under
    the MIT license.  This should be compatible with the license
    of Pmw, and will be discussed on the list.

--- __init__.py
+++ __init__.py
@@ -1,20 +1,24 @@
-# Maintainer: Friedrich Romstedt <friedrichromstedt@gmail.com>
-# Copyright 2009 Friedrich Romstedt
-#    This file is part of pmw2.
-#
-#    pmw2 is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    pmw2 is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with pmw2.  If not, see <http://www.gnu.org/licenses/>.
-# $Last changed: 2009 Sep 23$
+# Copyright (c) 2010 Friedrich Romstedt <www.friedrichromstedt.org>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Last changed: 2009 Sep 23
 # Developed since: Sep 2009
 # File version: 0.1.0b
 
